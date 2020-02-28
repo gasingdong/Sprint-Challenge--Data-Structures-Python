9158b9c4925625df6446bbeb294038f0fcfb8a96
Brandon Dong <gasingdong@gmail.com>
2020-02-28 08:54:28 -0800

    ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,14 +8,21 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if len(self.storage) >= self.capacity:
+            if not self.current:
+                self.current = self.storage.head
+            self.current.value = item
+            self.current = self.current.next \
+                if self.current.next else self.storage.head
+        else:
+            self.storage.add_to_tail(item)
 
     def get(self):
-        # Note:  This is the only [] allowed
         list_buffer_contents = []
-
-        # TODO: Your code here
-
+        current_node = self.storage.head
+        while current_node:
+            list_buffer_contents.append(current_node.value)
+            current_node = current_node.next
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
@@ -23,10 +30,14 @@ class RingBuffer:
 
 class ArrayRingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None for index in range(0, capacity)]
 
     def append(self, item):
-        pass
+        self.storage[self.current] = item
+        self.current = self.current + 1 \
+            if self.current < self.capacity - 1 else 0
 
     def get(self):
-        pass
+        return [value for value in self.storage if value]
